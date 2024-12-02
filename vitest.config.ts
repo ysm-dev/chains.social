dc90a291f680e54415f4a11c10311e03fe63f259
Gyujeong Hwang <octave08b@gmail.com>
2024-12-02 22:31:14 +0900

    Add Vitest for testing and coverage configuration

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vitest/config"
+
+export default defineConfig({
+  test: {
+    coverage: {
+      provider: "v8", // or 'v8'
+    },
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+})
