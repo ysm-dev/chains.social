f4da7d2f706632d62d6086a02d0ccc00dd1eab19
Gyujeong Hwang <octave08b@gmail.com>
2024-12-03 11:32:49 +0900

    Refactor tests for Discord and GitHub functions to remove mock fetch implementations

--- src/functions/discord/getDiscordInviteInfo.test.ts
+++ src/functions/discord/getDiscordInviteInfo.test.ts
@@ -8,19 +8,6 @@ describe("getDiscordInviteInfo function format tests", () => {
   it("should match the expected response format", async () => {
     const inviteId = "buildonbase"
 
-    global.fetch = vi.fn(() =>
-      Promise.resolve({
-        ok: true,
-        status: 200,
-        json: () =>
-          Promise.resolve({
-            code: "buildonbase",
-            approximate_member_count: 417420,
-            approximate_presence_count: 23545,
-          }),
-      } as Response),
-    )
-
     const response = await getDiscordInviteInfo(inviteId)
 
     expect(response.code).toBe(inviteId)
--- src/functions/github/getGithubOrganizationInfo.test.ts
+++ src/functions/github/getGithubOrganizationInfo.test.ts
@@ -1,24 +1,10 @@
 import { getGithubOrganizationInfo } from "@/functions/github/getGithubOrganizationInfo"
+import { isLocal } from "@/utils/isLocal"
 import { describe, expect, it, vi } from "vitest"
 
 describe("getGithubOrganizationInfo function format tests", () => {
   it("should match the expected response format", async () => {
     const organizationName = "base-org"
-
-    global.fetch = vi.fn(() =>
-      Promise.resolve({
-        ok: true,
-        status: 200,
-        json: () =>
-          Promise.resolve({
-            login: "base-org",
-            name: "Base",
-            followers: 4339,
-            public_repos: 121,
-            type: "Organization",
-          }),
-      } as Response),
-    )
 
     const response = await getGithubOrganizationInfo(organizationName)
 
