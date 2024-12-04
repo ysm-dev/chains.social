c4ecd9524cfdf84869943abedbfe019bebd3cff3
Gyujeong Hwang <octave08b@gmail.com>
2024-12-04 16:14:36 +0900

    Add validation for npm link format in retrieval functions

--- src/functions/getLastReleasedVersionFromNpm.ts
+++ src/functions/getLastReleasedVersionFromNpm.ts
@@ -2,6 +2,10 @@ import { getNpmPackageInfo } from "@/functions/npm/getNpmPackageInfo"
 import { getLastSegment } from "@/utils/getLastSegment"
 
 export const getLastReleasedVersionromNpm = async (npmLink: string) => {
+  if (!npmLink.startsWith("https://www.npmjs.com/package/")) {
+    throw new Error("Invalid npm link")
+  }
+
   const packageName = npmLink.replace("https://www.npmjs.com/package/", "")
 
   const data = await getNpmPackageInfo(packageName)
--- src/functions/getLastWeekDownloadsFromNpm.ts
+++ src/functions/getLastWeekDownloadsFromNpm.ts
@@ -2,6 +2,10 @@ import { getNpmLastWeekDownloads } from "@/functions/npm/getNpmLastWeekDownloads
 import { getLastSegment } from "@/utils/getLastSegment"
 
 export const getLastWeekDownloadsFromNpm = async (npmLink: string) => {
+  if (!npmLink.startsWith("https://www.npmjs.com/package/")) {
+    throw new Error("Invalid npm link")
+  }
+
   const packageName = npmLink.replace("https://www.npmjs.com/package/", "")
 
   const { downloads } = await getNpmLastWeekDownloads(packageName)
