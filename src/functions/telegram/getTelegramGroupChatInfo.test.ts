a4a8bd7c0969205245a9780770185f0e014e3305
Gyujeong Hwang <octave08b@gmail.com>
2024-12-05 13:55:49 +0900

    Add functions to retrieve member and online counts from Telegram

---
+++ src/functions/telegram/getTelegramGroupChatInfo.test.ts
@@ -0,0 +1,14 @@
+import { getMetadataFromCMC } from "@/functions/coinmarketcap/getMetadataFromCMC"
+import { getTelegramGroupChatInfo } from "@/functions/telegram/getTelegramGroupChatInfo"
+import { describe, expect, it } from "vitest"
+
+describe.concurrent("getTelegramGroupChatInfo", () => {
+  it("solana group chat", async () => {
+    const telegramSlug = "solana"
+
+    const response = await getTelegramGroupChatInfo(telegramSlug)
+
+    expect(response.memberCount).toBeGreaterThan(0)
+    expect(response.onlineCount).toBeGreaterThan(0)
+  })
+})
