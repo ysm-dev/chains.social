9a7a4b376ac74eadd352facce7fad94719213682
ysm-dev <i@ysm.dev>
2024-12-06 16:20:18 +0900

    Add YouTube channel retrieval function and tests; update cron job name and increase test timeout

---
+++ src/functions/youtube/getYoutubeChannelFromWebsite.test.ts
@@ -0,0 +1,53 @@
+import { getYoutubeChannelFromWebsite } from "@/functions/youtube/getYoutubeChannelFromWebsite"
+import { concurrent, map, pipe, toArray, toAsync } from "@fxts/core"
+import { describe, expect, it } from "vitest"
+
+const tests = [
+  {
+    website: "circle.com",
+    expected: `/c/Circlecryptofinance`,
+  },
+  {
+    website: "internetcomputer.org",
+    expected: `/dfinity`,
+  },
+  {
+    website: "thegraph.com",
+    expected: `/@GraphProtocol`,
+  },
+  {
+    website: "sui.io",
+    expected: `/@Sui-Network`,
+  },
+  {
+    website: "www.starknet.io",
+    expected: `/channel/UCnDWguR8mE2oDBsjhQkgbvg`,
+  },
+  {
+    website: "ethereum.org",
+    expected: null,
+  },
+  {
+    website: "solana.com",
+    expected: null,
+  },
+]
+
+describe.concurrent("getYoutubeChannelFromWebsite", async () => {
+  it("getYoutubeChannelFromWebsite", async () => {
+    const results = await pipe(
+      tests,
+      map(({ website }) => `https://${website}`),
+      toAsync,
+      map(getYoutubeChannelFromWebsite),
+      concurrent(tests.length),
+      toArray,
+    )
+
+    expect(results).toStrictEqual(
+      tests.map(({ expected }) =>
+        expected ? `https://www.youtube.com${expected}` : null,
+      ),
+    )
+  })
+})
