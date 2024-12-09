23ad1544e4c6788f334f945f27130fa602788efa
Gyujeong Hwang <octave08b@gmail.com>
2024-12-09 17:19:51 +0900

    Refactor getSimInfo function to use query parameters for API requests

--- src/functions/sim/getSimInfo.ts
+++ src/functions/sim/getSimInfo.ts
@@ -4,11 +4,14 @@ import { z } from "zod"
 
 export const getSimInfo = memoize(async (hostname: string) => {
   const response = await ofetch<GetSimInfoResponse>(
-    `https://data.similarweb.com/api/v1/data?domain=${hostname}`,
+    `https://data.similarweb.com/api/v1/data`,
     {
       headers: {
         "user-agent":
           "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+      },
+      query: {
+        domain: hostname,
       },
     },
   )
