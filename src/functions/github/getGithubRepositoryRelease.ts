b875917b5a9659440ab3e3e1e1b4040cdf1a29ff
Gyujeong Hwang <octave08b@gmail.com>
2024-12-14 14:14:31 +0900

    Add null check for response in getGithubRepositoryRelease function

--- src/functions/github/getGithubRepositoryRelease.ts
+++ src/functions/github/getGithubRepositoryRelease.ts
@@ -18,6 +18,10 @@ export const getGithubRepositoryRelease = memoize(
       },
     )
 
+    if (!response) {
+      return null
+    }
+
     return getGithubRepositoryReleaseSchema.parse(response)
   },
 )
